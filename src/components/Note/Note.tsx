3913352c94e82ccde100cf7e65c6135d90ec9df3
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-01-22 20:56:43 -0300

    :sparkles: feat: Notes Update

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,13 +1,47 @@
-export default function Note() {
+import { useState } from "react";
+import { CiStar } from "react-icons/ci";
+import { MdStar } from "react-icons/md";
+import { LuPaintBucket, LuPencil } from "react-icons/lu";
+import Modal from "react-modal";
+import "./styles.css"
+
+interface NoteProps {
+  title: string;
+  description: string;
+  isFavorite?: boolean;
+}
+
+export default function Note({ title, description, isFavorite }: NoteProps) {
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   return (
-    <div>
-      <div className="title">
-        <h1>Title</h1> <button>Start</button>
+    <div className="newNoteBox">
+      <div className="titleBox">
+        <p className="title">{title}</p>
+        <button className="starButton">
+          {!isFavorite ? <CiStar /> : <MdStar color="orange" />}
+        </button>
       </div>
-      <p>Content</p>
-      <div className="footerNote">
-        Footer
+      <div className="createNote">
+        <p className="text-gray-500">{description}</p>
       </div>
+      <section className="icons">
+        <LuPencil className="mr-5" size={20} />
+        <button onClick={() => setModalIsOpen(true)}>
+          <LuPaintBucket size={20} />
+        </button>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+        >
+          <button onClick={closeModal}>close</button>
+        </Modal>
+      </section>
     </div>
   );
 }
