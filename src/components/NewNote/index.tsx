4212db6820e19871c7b718bbc41b51bf0f5b0d67
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-02-13 15:23:06 -0300

    :sparkles: feat: New Note style finished

--- src/components/NewNote/index.tsx
+++ src/components/NewNote/index.tsx
@@ -1,17 +1,26 @@
 import { CiStar } from "react-icons/ci";
 import "./styles.css";
+import { MdStar } from "react-icons/md";
+import { useState } from "react";
+import { set } from "date-fns";
 
 export default function NewNote() {
+  
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
     <div className="newNoteBox">
       <div className="titleBox">
-        <p className="title">Title</p>
-        <button className="starButton">
-          <CiStar />
+        <input type="text" placeholder="Title" className="text-gray-500 w-[100%] outline-none border-none" />
+        <button className="starButton" onClick={() => setIsFavorite(!isFavorite)}>
+          {!isFavorite ? <CiStar /> : <MdStar color="orange" />}
         </button>
       </div>
       <div className="createNote">
-        <p className="text-gray-500">Criar nota...</p>
+        <textarea
+          placeholder="Description"
+          className="text-gray-500 w-[100%] h-[100%] resize-none outline-none border-none"
+        ></textarea>
       </div>
     </div>
   );
