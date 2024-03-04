0deb061f5ebd78cd77816b4ca4e30e66e5dfe97a
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-03-04 17:42:59 -0300

    :sparkles: feat: Edit and delete first version

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { CiStar } from "react-icons/ci";
 import { MdStar } from "react-icons/md";
+import { FaRegTrashCan } from "react-icons/fa6";
 import "./styles.css";
 
 interface NoteProps {
@@ -30,12 +31,34 @@ export default function Note({ id, title, content, isfavorite }: NoteProps) {
     window.location.reload();
   };
 
+  const handleEditClick = () => {
+    fetch(`http://localhost:3003/notes/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        content: content,
+        isfavorite: isStartFavorite,
+      }),
+    });
+  };
+
+  const handleDeleteClick = () => {
+    fetch(`http://localhost:3003/notes/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    window.location.reload();
+  };
+
   return (
     <div className="newNoteBox">
       <div className="titleBox">
-        <p className="title">
-          {title}
-        </p>
+        <p className="title">{title}</p>
         <button className="starButton" onClick={handleStarClick}>
           {!isStartFavorite ? <CiStar /> : <MdStar color="orange" />}
         </button>
@@ -43,10 +66,21 @@ export default function Note({ id, title, content, isfavorite }: NoteProps) {
       <div className="createNote">
         <textarea
           placeholder="Content for a new note"
-          value={content}
-          readOnly={true}
+          defaultValue={content}
           className="outline-none border-none w-[100%] h-[100%] resize-none"
+          onChange={(e) => {
+            content = e.target.value;
+            handleEditClick();
+          }}
         ></textarea>
+      </div>
+      <div className="bottomBox">
+        <button className="editButton" onClick={handleEditClick}>
+          Salvar Alterações
+        </button>
+        <button className="deleteButton" onClick={handleDeleteClick}>
+          <FaRegTrashCan />
+        </button>
       </div>
     </div>
   );
