e4ca284ab543f2bfb918c1ac29a95b2d76677e5a
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-03-07 13:50:24 -0300

    Notes app first version finished

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -40,20 +40,34 @@ export default function Note({ id, title, content, isfavorite }: NoteProps) {
     });
     window.location.reload();
   };
+  
 
   return (
-    <div className="newNoteBox">
-      <div className="titleBox">
-        <p className="title">{title}</p>
+    <div className={`newNoteBox`}>
+      <div className="titleBox bg-transparent">
+        <input className="title" defaultValue={title}
+        onChange={(e) => {
+          fetch(`http://localhost:3003/notes/${id}`, {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              title: e.target.value,
+              content: content,
+              isfavorite: isStartFavorite,
+            }),
+          });
+        }}/>
         <button className="starButton" onClick={handleStarClick}>
           {!isStartFavorite ? <CiStar /> : <MdStar color="orange" />}
         </button>
       </div>
-      <div className="createNote">
+      <div className="createNote bg-transparent">
         <textarea
           placeholder="Content for a new note"
           defaultValue={content}
-          className="outline-none border-none w-[100%] h-[100%] resize-none"
+          className="outline-none border-none w-[100%] h-[100%] resize-none bg-transparent"
           onChange={(e) => {
             fetch(`http://localhost:3003/notes/${id}`, {
               method: "PATCH",
@@ -69,8 +83,8 @@ export default function Note({ id, title, content, isfavorite }: NoteProps) {
           }}
         ></textarea>
       </div>
-      <div className="bottomBox">
-        <button className="deleteButton" onClick={handleDeleteClick}>
+      <div className="bottomBox bg-transparent">
+        <button className="deleteButton bg-transparent" onClick={handleDeleteClick}>
           <FaRegTrashCan />
         </button>
       </div>
