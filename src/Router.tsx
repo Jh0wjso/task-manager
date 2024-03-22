0453dd25c798336950461a9aaf3a228662472956
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-03-21 22:26:02 -0300

    :sparkles: feat: Login page first version

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Inicio from "./screens/Inicio";
+import LoginPage from "./screens/Login";
 
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+
+interface User {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //handleLogin
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch("http://localhost:3003/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("token", data.accessToken);
+      })
+      .catch((error) => console.error("Erro ao fazer login:", error));
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h1 className="pageTittle mt-10 text-center text-2xl font-bold leading-9 tracking-tight">CoreNotes</h1>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight pageTittle">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            <a
+              href="#"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Register Here
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+/*import React, { useState } from "react";
+
+interface User {
+  email: string;
+  password: string;
+  name: string;
+}
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //handleLogin
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch("http://localhost:3003/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("token", data.accessToken);
+      })
+      .catch((error) => console.error("Erro ao fazer login:", error));
+  };
+
+  return (
+    <div className="flex justify-center items-center w-screen h-screen" >
+      <form onSubmit={handleLogin} className="flex justify-center flex-col">
+        <label>
+          email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
+
+LoginPage;*/
