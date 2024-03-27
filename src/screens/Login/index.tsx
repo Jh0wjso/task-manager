b131094a1a415498bdc274732e456fd2348aec66
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-03-27 16:24:12 -0300

    :sparkles: feat: User register first version

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 interface User {
   email: string;
@@ -32,7 +33,9 @@ export default function LoginPage() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">CoreNotes</h1>
+          <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
+            CoreNotes
+          </h1>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">
             Sign in to your account
           </h2>
@@ -82,7 +85,6 @@ export default function LoginPage() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -101,20 +103,18 @@ export default function LoginPage() {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?{" "}
-            <a
-              href="#"
+            <Link
+              to="/register"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Register Here
-            </a>
+            </Link>
           </p>
         </div>
       </div>
     </>
   );
 }
-
-
 
 /*import React, { useState } from "react";
 
