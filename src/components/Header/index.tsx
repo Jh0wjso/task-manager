f17206fd7b2e6ff1c788c47755bbf6bc6e8928bd
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-04-18 15:19:44 -0300

    :lipstick: feat: Styles and header responsively

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,16 @@
 import { Link } from "react-router-dom";
-import { BsSearch } from "react-icons/bs";
 import "./styles.css";
-import { useEffect, useState } from "react";
-import { NoteProps } from "../../data/interfaces/noteProps.props";
 import UserModal from "../UserModal";
 import { FaRegUserCircle } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Header() {
-  const [searchResults, setSearchResults] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
     <nav className="containerHeader">
       <div className="navbarContent">
-        <Link to="/" className="flex flex-row items-center">
+        <Link to="/home" className="flex flex-row items-center">
           <h1 className="pageTittle">CoreNotes</h1>
         </Link>
         <div className="flex flex-col items-start">
--- src/screens/Inicio/index.tsx
+++ src/screens/Inicio/index.tsx
@@ -2,12 +2,14 @@ import "./styles.css";
 
 import Home from "../../components/Home";
 import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function Inicio() {
   return (
     <div className="homeContent">
       <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
